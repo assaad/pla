b90a77dd142361e7719bb0b30eb26200217f75f2
Assaad <assaad.moawad@uni.lu>
2013-09-18 12:00:49 +0200

    added sensor

---
+++ lu.snt.serval.pla.sensors/src/main/java/lu/snt/serval/pla/sensors/TempListener.java
@@ -0,0 +1,12 @@
+package lu.snt.serval.pla.sensors;/*
+* Author : Gregory Nain (developer.name@uni.lu)
+* Date : 06/12/12
+* (c) 2012 University of Luxembourg – Interdisciplinary Centre for Security Reliability and Trust (SnT)
+* All rights reserved
+*/
+
+
+
+public interface TempListener {
+    void temperatureUpdated(TempRecord record);
+}
